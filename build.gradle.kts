2583b14773993ae5eb7dd1a543e18243a1dca5dc
Linus Törnkrantz <linus@prettybyte.com>
2026-05-27 17:30:13 +0200

    klerk version

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
     `maven-publish`
 }
 
-val klerkVersion = "1.0.0-beta.7-SNAPSHOT"
+val klerkVersion = "a3640ba3a8"
 val ktorVersion = "3.2.3"
 val gsonVersion = "2.9.0"
 val kotlinLoggingVersion = "2.1.21"
